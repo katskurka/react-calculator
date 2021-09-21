a577aee8d51fb16e7a84094b1a40f798ee30306d
katskurka <kati.skurka@gmail.com>
2021-09-21 16:04:51 -0400

    added calculator function, still not working

---
+++ src/components/calculate.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const Calculate = () => {
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [operator, setOperator] = useState('')
+  const [result, setResult] = useState('')
+
+  const magicmath = (num1, operator, num2) => {
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+
+    if (operator === '+') {
+      return num1 + num2
+    } else if (operator === '-') {
+      return num1 - num2
+    } else if (operator === '*') {
+      return num1 * num2
+    } else if (operator === '/') {
+      return num1 / num2
+    } else {
+      return 'input invalid'
+    }
+  }
+
+  return (
+    <>
+    <div className="calculator">
+      <input type="text" name="num1" onChange={event => setNum1(event.target.value)} value={num1} />
+      <select name="operator" onChange={event => setOperator(event.target.value)} className="operator" value={operator}>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+      <input type="text" name="num2" onChange={event => { setNum2(event.target.value) }} value={num2} />
+      <button onClick={event => setResult(magicmath(num1, operator, num2))}>=</button>
+      <input type="text" onChange={event => setResult(event.target.value)} value={result} />
+    </div>
+    </>
+  )
+}
+
+export default Calculate
